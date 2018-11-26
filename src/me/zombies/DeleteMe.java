b1622fd720dfcc6a426b1f6f9ccc6dbb50a512dd
Emily <emily.c.brereton@gmail.com>
2018-11-26 15:10:25 -0500

    Test push of Java from Eclipse

---
+++ src/me/zombies/DeleteMe.java
@@ -0,0 +1,10 @@
+package me.zombies;
+
+public class DeleteMe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//trynig to push a new java file via gitkraken to Githubs
+	}
+
+}
