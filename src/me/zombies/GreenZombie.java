518c5b434b8536de3b5b08ae82c81cbac67ecb6a
Emily <emily.c.brereton@gmail.com>
2018-11-28 13:14:03 -0500

    Zombie Setup Etc

---
+++ src/me/zombies/GreenZombie.java
@@ -0,0 +1,22 @@
+package me.zombies;
+
+import java.util.Random;
+
+public class GreenZombie {
+
+	private Random rand = new Random();
+
+	int health = 6;
+	int speed = 3;
+	int resistance = 4; 
+	int damage = 4;
+	
+	int location;
+	
+	GreenZombie(){
+		//location = rand ----- ;
+	}
+	
+
+
+}
