36577e1281d6ab6fe521dfdb4eb64606f4ea3acd
emilyclara <emily.c.brereton@gmail.com>
2018-12-10 12:30:38 -0500

    Zombies move!

--- src/me/zombies/Main.java
+++ src/me/zombies/Main.java
@@ -25,15 +25,17 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
+//import animate.Circle;
+
 public class Main implements KeyListener, MouseListener, MouseMotionListener {
-	
-//JFrame and JWindow Creations
-	final static int WIN = 1500;
+
+	//JFrame and JWindow Creations
+	final static int WIN = 500;
 	static JFrame window;
 	DrawingPanel drPanel = new DrawingPanel();
 
-	
-//Colors, Font, and Stroke 
+
+	//Colors, Font, and Stroke 
 	Color White = new Color (255, 255, 255);
 	Color Blue = new Color (0, 0, 255);
 	Color Red = new Color (255, 0, 0);
@@ -41,42 +43,44 @@ public class Main implements KeyListener, MouseListener, MouseMotionListener {
 	Font HPBarFont = new Font("Arial", Font.PLAIN, WIN/30);
 	Font ZombiesCounterFont = new Font("Arial", Font.BOLD, WIN/15);
 	BasicStroke PlayerHPBarStroke = new BasicStroke(WIN/300);
-	
-//Constants for Zombie Types
-	
+
+	//Constants for Zombie Types
+
 	int GreenZombie = 1;
 	int BlueZombie =  2;
 	int RedZombie =   3;
 	int GoldZombie =  4;
-	
-//Global Variables
+
+	//Global Variables
 	static PlayerStats Player = new PlayerStats("Josh");	// <---- Creating the Player Object
-	
-	
-	
+
+
+
 	ArrayList<Zombies> zombies = new ArrayList<Zombies>();
 	int Round = 0;
 	int ZombiesCounter = 0;
 	Zombies z;
-	
+
 	static int mouseX;	// <---- Mouse Variables
 	static int mouseY;
-	
+
 	static boolean W = false,	// <---- Input variables for the player.
-			       A = false,	//		 These variables are set to false,
-			       S = false,	//       when the key is pressed or mouse
-			       D = false,	//		 button is clicked, the corresponding
-			      M1 = false,	//		 variable is set to true
-			      M2 = false;
-	
+			A = false,	//		 These variables are set to false,
+			S = false,	//       when the key is pressed or mouse
+			D = false,	//		 button is clicked, the corresponding
+			M1 = false,	//		 variable is set to true
+			M2 = false;
+
 	Timer timer;	// <---- Initializes the Timer
 	int tSpeed = 1;	// <---- The Timer's Speed
 	long lastHit = 0L;
-	
+
 	public static void main (String [] args) {new Main();}
-	
+
+	final static int T2_SPEED = 10;
+
 	Main() {
-	//JFrame Setup:
+		//JFrame Setup:
 		window = new JFrame("Zombie Game");						// <---- Sets the titles
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	// <---- Allows for closing
 		window.setResizable(false);								// <---- Turns of resizing
@@ -86,263 +90,273 @@ public class Main implements KeyListener, MouseListener, MouseMotionListener {
 		window.add(drPanel);									// <---- Adds the drPanel to the Window
 		window.pack();											// <---- Packs the Window
 		window.setVisible(true);								// <---- Sets it visable
-	
+
 		timer = new Timer(tSpeed, new TimerListener());			// <---- Creates the Timer
 		timer.start();											// <---- Starts the Timer
+
+		Timer moveTimer = new Timer(T2_SPEED, new ActionListener() { //this was done another way in class
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				moveZombies();
+				drPanel.repaint();
+			}
+		});
+		moveTimer.start();
 	}
-	
+
 	@SuppressWarnings("serial")
 	private class DrawingPanel extends JPanel {
 		DrawingPanel() {
 			this.setPreferredSize(new Dimension (WIN, WIN));	// <---- Sets the Size
 			this.setBackground(White);							// <---- Sets the background color
 		}
-		
+
 		@Override
 		public void paintComponent(Graphics g) {
 			Graphics2D g2 = (Graphics2D) g;	
 			g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			
+
 			super.paintComponent(g);
 			this.requestFocus();
-			
+
 			drawPlayer(g, g2);
-			
-		//Draw Zombies
+
+			//Draw Zombies
 			for (Zombies z: zombies) {
+				System.out.println(z);
 				z.paint(g, g2);
 			}
-		
+
 			drawPlayerHealthBar(g, g2);
-			
+
 			drawZombieCounter(g, g2);
-			
+
 		}		
 
 	}
-	
-//Draw the Player
+
+	//Draw the Player
 	void drawPlayer(Graphics g, Graphics2D g2) {
-		
+
 		BufferedImage PlayerImg = null;
 		try { PlayerImg = ImageIO.read(new File("Player.png")); 	// <---- Loads the player Sprite file
 		} catch (IOException e) {}
-		
+
 		g.setColor(Black);
 		g.fillRect(Player.x, Player.y,Player.width, Player.height);
-		
+
 		g2.rotate(Math.toRadians(Player.angle), Player.x+(Player.width/2), Player.y+(Player.height/2));		// <---- Rotates the whole screen	
 		g.drawImage(PlayerImg, Player.x, Player.y,Player.width, Player.height, drPanel);					// <---- Draws the Player
 		g2.rotate(Math.toRadians(-Player.angle), Player.x+(Player.width/2), Player.y+(Player.height/2));	// <---- Rotates the whole screen back
-	
+
 	}
 
-//Move the Player
+	//Move the Player
 	void movePlayer() {
-		
+
 		double vx;
 		double vy;
 		double moveAngle;
-		
+
 		if (W) {								// <---- Moving Forward
-			
+
 			moveAngle = Math.toRadians(Player.angle-90);
-			
+
 			vx = Player.speed*Math.cos(moveAngle);	// <----Gets the horizontal speed based off the angle
 			vy = Player.speed*Math.sin(moveAngle);	// <----Gets the  vertical  speed based off the angle
-			
+
 			Player.y+=vy; 	// <---- Moves the player in accordance with
 			Player.x+=vx;	//		 it's vertical and horizontal speeds
 		}
-		
+
 		if (S) {								// <---- Moving Back
-			
+
 			moveAngle = Math.toRadians(Player.angle+90);
-			
+
 			vx = Player.speed*Math.cos(moveAngle);
 			vy = Player.speed*Math.sin(moveAngle);
-			
+
 			Player.y+=vy;
 			Player.x+=vx;
 		}
-		
+
 		if (D) {								// <---- Circling Right (Counter Clockwise)
-			
+
 			moveAngle = Math.toRadians(Player.angle);
-			
+
 			vx = Player.speed*Math.cos(moveAngle);
 			vy = Player.speed*Math.sin(moveAngle);
-			
+
 			Player.y+=vy;
 			Player.x+=vx;
-			
+
 		}
-		
+
 		if (A) {								// <---- Circling Left (Clockwise)
-			
+
 			moveAngle = Math.toRadians(Player.angle-180);
-			
+
 			vx = Player.speed*Math.cos(moveAngle);
 			vy = Player.speed*Math.sin(moveAngle);
-			
+
 			Player.y+=vy;
 			Player.x+=vx;
 		}	
-		
+
 	}
-	
-//Draw the Health Bar
+
+	//Draw the Health Bar
 	void drawPlayerHealthBar(Graphics g, Graphics2D g2) {
 
 		g2.setStroke(PlayerHPBarStroke);
-		
+
 		int BarWidth = WIN/3; 	// <---- Constant Ratios based off of the Screen Width
 		int BarHeight = WIN/30;
-		
+
 		g.setColor(White);										// <---- White Background
 		g.fillRect(BarHeight, BarHeight, BarWidth, BarHeight);
-		
+
 		g.setColor(Red);										// <---- Red Health Meter
 		int HPBarWidth = (int) (Player.PercentHP*BarWidth); 	// <---- The Size of the Meter based of the Health Precentage
 		g.fillRect(BarHeight, BarHeight, HPBarWidth, BarHeight);
-		
+
 		g.setColor(Black);										// <---- Black Boarder
 		g.drawRect(BarHeight, BarHeight, BarWidth, BarHeight);
-		
+
 		g2.setFont(HPBarFont);									// <---- Display Text of HP
 		String HPString = Player.HP+"/"+Player.maxHP;
 		g.drawString(HPString, (int)(WIN/3.6), WIN/16);
-	
+
 	}
 
-//Draw the Zombie Counter
+	//Draw the Zombie Counter
 	void drawZombieCounter(Graphics g, Graphics2D g2) {
 
 		FontMetrics fontMetrics = g2.getFontMetrics(ZombiesCounterFont);	// <---- Creates a FontMetrics
-		
+
 		String str = null;									// <---- Initizalises the string
-		
+
 		if (ZombiesCounter<10) str = "0"+ZombiesCounter;  	// <---- Puts a '0' in front of single-digit numers
 		else str = ""+ZombiesCounter;
-		
+
 		g.setColor(Black);			
 		g2.setFont(ZombiesCounterFont);			// <---- Changes the font
-		
+
 		int rightAlign = (WIN/30)*29; 			// <---- Creates a variable based off the screen width to align the text to the right
-		
+
 		g.drawString(str, (rightAlign - fontMetrics.stringWidth(str)), WIN/16);
-		
+
 	}
-	
-//Add Zombies
+
+	//Add Zombies
 	void addZombies() {
 		Round++;
 		ZombiesCounter = 0;
-		
+
 		//Round 1 Zombie Adding
-			if (Round==1) {
-				for (int i=0; i<45; i++) zombies.add(new Zombies(GreenZombie));
-				ZombiesCounter = 45;
-			}
-			
+		if (Round==1) {
+			for (int i=0; i<45; i++) zombies.add(new Zombies(GreenZombie));
+			ZombiesCounter = 45;
+		}
+
 		//Round 2 Zombie Adding
-			if (Round==2) {
-				for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<10; i++) zombies.add(new Zombies(BlueZombie));
-				ZombiesCounter = 45;
-			}
-			
+		if (Round==2) {
+			for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<10; i++) zombies.add(new Zombies(BlueZombie));
+			ZombiesCounter = 45;
+		}
+
 		//Round 3 Zombie Adding
-			if (Round==3) {
-				for (int i=0; i<40; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<20; i++) zombies.add(new Zombies(BlueZombie));
-				ZombiesCounter = 60;
-			}
+		if (Round==3) {
+			for (int i=0; i<40; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<20; i++) zombies.add(new Zombies(BlueZombie));
+			ZombiesCounter = 60;
+		}
 		//Round 4 Zombie Adding
-			if (Round==4) {
-				for (int i=0; i<30; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<35; i++) zombies.add(new Zombies(BlueZombie));
-				ZombiesCounter = 65;
-			}
-			
+		if (Round==4) {
+			for (int i=0; i<30; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<35; i++) zombies.add(new Zombies(BlueZombie));
+			ZombiesCounter = 65;
+		}
+
 		//Round 5 Zombie Adding
-			if (Round==5) {
-				for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i< 1; i++) zombies.add(new Zombies(RedZombie));
-				ZombiesCounter = 61;
-			}
-			
+		if (Round==5) {
+			for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i< 1; i++) zombies.add(new Zombies(RedZombie));
+			ZombiesCounter = 61;
+		}
+
 		//Round 6 Zombie Adding
-			if (Round==6) {
-				for (int i=0; i<40; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<30; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i< 5; i++) zombies.add(new Zombies(RedZombie));
-				ZombiesCounter = 75;
-			}			
-		
+		if (Round==6) {
+			for (int i=0; i<40; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<30; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i< 5; i++) zombies.add(new Zombies(RedZombie));
+			ZombiesCounter = 75;
+		}			
+
 		//Round 7 Zombie Adding
-			if (Round==7) {
-				for (int i=0; i<45; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i<15; i++) zombies.add(new Zombies(RedZombie));
-				ZombiesCounter = 85;
-			}
-			
+		if (Round==7) {
+			for (int i=0; i<45; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i<15; i++) zombies.add(new Zombies(RedZombie));
+			ZombiesCounter = 85;
+		}
+
 		//Round 8 Zombie Adding
-			if (Round==8) {
-				for (int i=0; i<25; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<10; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i<05; i++) zombies.add(new Zombies(RedZombie));
-				for (int i=0; i<01; i++) zombies.add(new Zombies(GoldZombie));
-				ZombiesCounter = 41;
-			}
-			
+		if (Round==8) {
+			for (int i=0; i<25; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<10; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i<05; i++) zombies.add(new Zombies(RedZombie));
+			for (int i=0; i<01; i++) zombies.add(new Zombies(GoldZombie));
+			ZombiesCounter = 41;
+		}
+
 		//Round 9 Zombie Adding
-			if (Round==9) {
-				for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i<15; i++) zombies.add(new Zombies(RedZombie));
-				for (int i=0; i<05; i++) zombies.add(new Zombies(GoldZombie));
-				ZombiesCounter = 80;
-			}
-			
+		if (Round==9) {
+			for (int i=0; i<35; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<25; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i<15; i++) zombies.add(new Zombies(RedZombie));
+			for (int i=0; i<05; i++) zombies.add(new Zombies(GoldZombie));
+			ZombiesCounter = 80;
+		}
+
 		//Round 10 Zombie Adding
-			if (Round==10) {
-				for (int i=0; i<50; i++) zombies.add(new Zombies(GreenZombie));
-				for (int i=0; i<35; i++) zombies.add(new Zombies(BlueZombie));
-				for (int i=0; i<25; i++) zombies.add(new Zombies(RedZombie));
-				for (int i=0; i<10; i++) zombies.add(new Zombies(GoldZombie));
-				ZombiesCounter = 120;
-			}
-			
+		if (Round==10) {
+			for (int i=0; i<50; i++) zombies.add(new Zombies(GreenZombie));
+			for (int i=0; i<35; i++) zombies.add(new Zombies(BlueZombie));
+			for (int i=0; i<25; i++) zombies.add(new Zombies(RedZombie));
+			for (int i=0; i<10; i++) zombies.add(new Zombies(GoldZombie));
+			ZombiesCounter = 120;
+		}
+
 	}
-	
-//Timer Listener
+
+	//Timer Listener
 	private class TimerListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
-		
-	// ------------------------------------------------------
-	// ----- Stuff that happens every frame of the game -----
-	// ------------------------------------------------------	
+
+
+			// ------------------------------------------------------
+			// ----- Stuff that happens every frame of the game -----
+			// ------------------------------------------------------	
 			Player.PercentRatio = ((Player.HP*100)/Player.maxHP);
 			Player.PercentHP = Player.PercentRatio/100;
-			
-			
+
+
 			movePlayer();
-			
-		//Rotation of Player
+
+			//Rotation of Player
 			int deltaX = mouseX-Player.x; 				// <---- Subtracting the Player location 
 			int deltaY = mouseY-Player.y;				//		 from the Mouse Location
-			
+
 			Player.angle = Math.toDegrees(Math.atan2(deltaY, deltaX))+90; 	// <---- The angle of rotation
-			
-		//Player Death Check
+
+			//Player Death Check
 			if (Player.HP<=0) Player.alive = false;
-			
-		//Zombie Hit Check
+
+			//Zombie Hit Check
 			for (Zombies z: zombies) {
 				if (z.intersects(Player)) {
 					long now = System.currentTimeMillis();
@@ -352,79 +366,94 @@ public class Main implements KeyListener, MouseListener, MouseMotionListener {
 					}
 				}
 			}
-			
-		//Zombie Death Check
+
+			//Zombie Death Check
 			for (Zombies z: zombies) {
 				if (z.HP<=0) {
 					zombies.remove(z);
 					ZombiesCounter--;
 				}
 			}
-			
-		//Zombies Round Check
+
+			//Zombies Round Check
 			if (ZombiesCounter<=0) addZombies();
-			
-		//Repaints the window (every frame)
+
+			//Repaints the window (every frame)
 			window.repaint();
 		}
-		
+
 	}	
 
-// -------------------------
-// ----- Player Inputs -----
-// -------------------------
-	
-//Keyboard Presses
-	
+	// -------------------------
+	// ----- Player Inputs -----
+	// -------------------------
+
+	//Keyboard Presses
+
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
+
 		if (e.getKeyCode()==KeyEvent.VK_W) W = true;
 		if (e.getKeyCode()==KeyEvent.VK_A) A = true;
 		if (e.getKeyCode()==KeyEvent.VK_S) S = true;
 		if (e.getKeyCode()==KeyEvent.VK_D) D = true;	
-		
+
 	}
-	
+
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+
 		if (e.getKeyCode()==KeyEvent.VK_W) W = false;
 		if (e.getKeyCode()==KeyEvent.VK_A) A = false;
 		if (e.getKeyCode()==KeyEvent.VK_S) S = false;
 		if (e.getKeyCode()==KeyEvent.VK_D) D = false;	
-		
+
 	}
-	
-//Mouse Inputs
-	
+
+	//Mouse Inputs
+
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
-		if (e.getButton()==MouseEvent.BUTTON1) M1 = true;
-		
+
+		if (e.getButton()==MouseEvent.BUTTON1) M1 = true;	
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+
 		if (e.getButton()==MouseEvent.BUTTON1) M1 = false;
-		
 	}
-	
+
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		mouseX = e.getX();
 		mouseY = e.getY();
 	}
-	
+
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		mouseX = e.getX();
 		mouseY = e.getY();
 	}
 
-//UNUSED METHOD
+	private void moveZombies() {
+		for(Zombies z : zombies) {
+			System.out.println(z.vx);
+			System.out.println(z.vy);
+			z.x += z.vx;
+			z.y += z.vy;
+			
+			//bounce off walls
+			//set constant buffer size at top
+			//Buffer image = 50 --> map walls
+/*			if (c.cx-c.r < Buffer) c.vx *= -1; east = width of window + buffer
+			if (c.cy-c.r < 0) c.vy *= -1; south = window height - buffer
+			if (c.cx+c.r > panW) c.vx *= -1;
+			if (c.cy+c.r > panH) c.vy *= -1;*/
+		}
+		//window.repaint();
+	}
+	//UNUSED METHOD
 	public void keyTyped(KeyEvent arg0) {}
 	public void mouseClicked(MouseEvent arg0) {}
 	public void mouseEntered(MouseEvent arg0) {}
--- src/me/zombies/Zombies.java
+++ src/me/zombies/Zombies.java
@@ -8,121 +8,155 @@ import java.awt.Rectangle;
 
 class Zombies extends Rectangle{
 
-//Colors
+	//Colors
 	Color White = new Color (255, 255, 255);
 	Color Blue = new Color (0, 0, 255);
 	Color Red = new Color (255, 0, 0);
 	Color Black = new Color (0, 0, 0);
 	BasicStroke ZombiesHPBarStroke = new BasicStroke(Main.WIN/500);
-	
-//Zombies Type
+
+	//Zombies Type
 	int GREEN = 1;
 	int BLUE = 2;
 	int RED = 3;
 	int GOLD = 4;
-	
-	int ZombiesWidth = Main.WIN/30;
-	
-//Variables
-//	int x, y;
-	
-//	int width, height;
-	int type;
-	
-	boolean alive;
-	
-	int HP, maxHP, speed, damage;
-	double PercentRatio, PercentHP, angle;
-	
-	Zombies(int type){
-		
-	//TODO: Create Zombie Behaviour Here
-		
-		
 
-	// Creating GREEN Zombies
+	int ZombiesWidth = Main.WIN/30;
+
+	//Variables
+	//int cx, cy; //centre coordinates
+	int vx, vy; //speeds
+
+	//	int width, height;
+	int type;
+
+	boolean alive;
+
+	int HP, maxHP, damage;
+	double PercentRatio, PercentHP, angle;
+
+	Zombies(int type){
+
+		//TODO: Create Zombie Behaviour Here
+
+
+
+		// Creating GREEN Zombies
 		if (type==GREEN) {					
 			this.maxHP = 10;
 			this.HP = 10;
-			this.speed = Main.WIN/1500;
+			//this.speed = Main.WIN/1500;
+			vx = (int)(Math.random()*2) + 8;
+			if(Math.random() > 0.5) {
+				vx = vx * -1;
+			}
+
+			vy = (int)(Math.random()*2) + 8;
+			if(Math.random() > 0.5) {
+				vy = vy * -1;
+			}
 			this.damage = 1;
 		}
-		
-	// Creating BLUE Zombies	
+
+		// Creating BLUE Zombies	
 		if (type==BLUE) {					
 			this.maxHP = 20;
 			this.HP = 20;
-			this.speed = Main.WIN/750;
+			vx = (int)(Math.random()*2) + 6;
+			if(Math.random() > 0.5) {
+				vx = vx * -1;
+			}
+
+			vy = (int)(Math.random()*2) + 6;
+			if(Math.random() > 0.5) {
+				vy = vy * -1;
+			}			
 			this.damage = 5;
 		}
-		
-	// Creating RED Zombies
+
+		// Creating RED Zombies
 		if (type==RED) {					
 			this.maxHP = 50;
 			this.HP = 50;
-			this.speed = Main.WIN/525;
+			vx = (int)(Math.random()*2) + 4;
+			if(Math.random() > 0.5) {
+				vx = vx * -1;
+			}
+
+			vy = (int)(Math.random()*2) + 4;
+			if(Math.random() > 0.5) {
+				vy = vy * -1;
+			}			
 			this.damage = 10;
 		}
-		
-	// Creating GOLD Zombies		
+
+		// Creating GOLD Zombies		
 		if (type==GOLD) {					
 			this.maxHP = 100;
 			this.HP = 100;
-			this.speed = Main.WIN/300;
+			vx = (int)(Math.random()*2) + 2;
+			if(Math.random() > 0.5) {
+				vx = vx * -1;
+			}
+
+			vy = (int)(Math.random()*2) + 2;
+			if(Math.random() > 0.5) {
+				vy = vy * -1;
+			}			
 			this.damage = 20;
 		}
-		
+
 		this.x = (int)(Math.random()*Main.WIN);
 		this.y = (int)(Math.random()*Main.WIN);
 		this.width = ZombiesWidth;
 		this.height = ZombiesWidth;
-		
+
 		this.type=type;
-		
+
 		this.PercentRatio = ((this.HP*100)/this.maxHP);
 		this.PercentHP = PercentRatio/100;
-		
+
 	}
-	
+
 	void paint(Graphics g, Graphics2D g2) {
-		
+
 		g.setColor(Black);
 		g.fillRect(x, y, ZombiesWidth, ZombiesWidth);
-		
+
 		if (this.type==GREEN) g.setColor(Color.GREEN);
 		if (this.type== BLUE) g.setColor(Color.BLUE);
 		if (this.type==  RED) g.setColor(Color.RED);
 		if (this.type== GOLD) g.setColor(Color.YELLOW);
-	
+
 		g.fillOval(x, y, ZombiesWidth, ZombiesWidth);
-		
+
 		drawZombiesHealthBar(g, g2, type);
 	}
-	
+
 	void drawZombiesHealthBar (Graphics g, Graphics2D g2, int type) {
 		g2.setStroke(ZombiesHPBarStroke);
-		
+
 		int BarWidth = Main.WIN/10; 	// <---- Constant Ratios based off of the Screen Width
 		int BarHeight = Main.WIN/150;
-		
+
 		int BarX = x+(ZombiesWidth/2)-(BarWidth/2);
 		int BarY = y-(Main.WIN/60);
-		
+
 		g.setColor(White);										// <---- White Background
 		g.fillRect(BarX, BarY, BarWidth, BarHeight);
-		
-		
+
+
 		if (this.type==GREEN) g.setColor(Color.GREEN);			// <---- Colored Health Meter
 		if (this.type== BLUE) g.setColor(Color.BLUE);
 		if (this.type==  RED) g.setColor(Color.RED);
 		if (this.type== GOLD) g.setColor(Color.YELLOW);
-					
+
 		int HPBarWidth = (int) (PercentHP*BarWidth); 			// <---- The Size of the Meter based of the Health Precentage
 		g.fillRect(BarX, BarY, HPBarWidth, BarHeight);
-		
+
 		g.setColor(Black);										// <---- Black Boarder
 		g.drawRect(BarX, BarY, BarWidth, BarHeight);		
-		
+
 	}
 
 }
